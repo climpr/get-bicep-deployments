822fb4f77fbdbfc3fd85d604014ce6ea4a2c380d
Audun Akse <audhage@gmail.com>
2024-11-24 10:31:01 +0100

    Add pester test

--- src/tests/Get-BicepDeployments.tests.ps1
+++ src/tests/Get-BicepDeployments.tests.ps1
@@ -10,11 +10,11 @@ Describe "Get-BicepDeployments.ps1" {
     Context "When mode is 'Modified'" {
         BeforeAll {
             $script:param = @{
-                Quiet                       = $true
-                EventName                   = "push"
-                Mode                        = "Modified"
-                DeploymentsRootDirectory    = "$mockDirectory/deployments"
-                ChangedFiles                = @(
+                Quiet                    = $true
+                EventName                = "push"
+                Mode                     = "Modified"
+                DeploymentsRootDirectory = "$mockDirectory/deployments"
+                ChangedFiles             = @(
                     "$mockDirectory/deployments/workload-local/.bicep/submodule.bicep"
                     "$mockDirectory/deployments/workload-remote-param/prod.bicepparam"
                 )
@@ -33,10 +33,10 @@ Describe "Get-BicepDeployments.ps1" {
     Context "When no filters are aplied" {
         BeforeAll {
             $script:param = @{
-                Quiet                       = $true
-                EventName                   = "schedule"
-                Mode                        = "All"
-                DeploymentsRootDirectory    = "$mockDirectory/deployments"
+                Quiet                    = $true
+                EventName                = "schedule"
+                Mode                     = "All"
+                DeploymentsRootDirectory = "$mockDirectory/deployments"
             }
 
             $script:res = ./src/Get-BicepDeployments.ps1 @param
@@ -54,12 +54,12 @@ Describe "Get-BicepDeployments.ps1" {
     Context "With pattern filters applied" {
         BeforeAll {
             $script:param = @{
-                Quiet                       = $true
-                EventName                   = "workflow_dispatch"
-                Mode                        = "All"
-                DeploymentsRootDirectory    = "$mockDirectory/deployments"
-                Pattern                     = "workload-remote-.+"
-                Environment                 = "prod"
+                Quiet                    = $true
+                EventName                = "workflow_dispatch"
+                Mode                     = "All"
+                DeploymentsRootDirectory = "$mockDirectory/deployments"
+                Pattern                  = "workload-remote-.+"
+                Environment              = "prod"
             }
 
             $script:res = ./src/Get-BicepDeployments.ps1 @param
@@ -69,6 +69,29 @@ Describe "Get-BicepDeployments.ps1" {
             $res | Should -HaveCount 2
             $res.Name | Should -Contain "workload-remote-modules-prod"
             $res.Name | Should -Contain "workload-remote-param-prod"
+        }
+    }
+
+    Context "With conflicting deploymentconfig files" {
+        BeforeAll {
+            $script:deploymentPath = "$mockDirectory/deployments/workload-multi-deploymentconfig"
+            $script:param = @{
+                Quiet                    = $true
+                EventName                = "workflow_dispatch"
+                Mode                     = "All"
+                DeploymentsRootDirectory = "$mockDirectory/deployments"
+                Pattern                  = "workload-multi-deploymentconfig"
+                Environment              = "dev"
+            }
+            Copy-Item -Path "$deploymentPath/deploymentconfig.json" -Destination "$deploymentPath/deploymentconfig.jsonc"
+        }
+
+        It "Should throw 'Found multiple deploymentconfig files.'" {
+            { ./src/Get-BicepDeployments.ps1 @param } | Should -Throw "*Found multiple deploymentconfig files.*"
+        }
+
+        AfterAll {
+            Remove-Item -Path "$script:deploymentPath/deploymentconfig.jsonc" -Confirm:$false
         }
     }
 }
